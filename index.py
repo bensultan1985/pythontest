dab8234e8f5371bfd145222be29c3c6fda46b091
bens <bensultan1985@gmail.com>
2021-08-28 15:37:47 -0700

    added loops

--- index.py
+++ index.py
@@ -1,2 +1,20 @@
+# This file will contain examples of Python functionality for my own reference.
+
+#print
 print('hello world')
-print('hi')
+
+#print a var
+var_one = 'a string'
+print(var_one)
+
+#loop - white space matters
+
+##index
+names = ['Ben', 'Sue', 'Phil']
+for name in range(len(names)):
+    print(names[name])
+
+##content of index
+names = ['Ben', 'Sue', 'Phil']
+for name in names:
+    print(name)
